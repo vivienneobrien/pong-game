f8299bc5339e06f182519bf80b2d7ae897ec2e36
vxo910 <vxo910@student.bhamclear>
2020-04-07 17:30:34 +0100

    comments

--- src/Game.java
+++ src/Game.java
@@ -4,13 +4,11 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Bounds;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -27,33 +25,34 @@ public class Game extends Application {
 		launch(args);
 	}
 
+	/**
+	 * Sets the game up using two players and a ball
+	 * 
+	 * @param primaryStage
+	 */
 	public void setUp(Stage primaryStage) {
 		root = new Group();
-
-		// CENTRE X, CENTRE Y, RADIUS X, RADIUS Y
 		ball = new Circle(10, Color.WHITE);
 		ball.relocate(200, 200);
 		player1 = new Rectangle(20, 200, PlayerParameters.radiusX, PlayerParameters.radiusY);
 		player1.setFill(Color.BLUE);
 		player2 = new Rectangle(370, 200, PlayerParameters.radiusX, PlayerParameters.radiusY);
 		player2.setFill(Color.BLUE);
-
-		root.getChildren().add(ball);
-		root.getChildren().add(player1);
-		root.getChildren().add(player2);
+		root.getChildren().addAll(ball, player1, player2);
 		scene = new Scene(root, 400, 400, Color.BLACK);
 		primaryStage.setTitle("Pong-Game");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-
 	}
 
+	/**
+	 * Start method is used set up the game for when the user opens the window
+	 */
 	public void startGame() {
-
+		/**
+		 * Key up
+		 */
 		scene.setOnKeyPressed(e -> {
-			/**
-			 * key up
-			 */
 			if (e.getCode() == KeyCode.UP) {
 				if (PlayerConstraints.checkLowerBound(player1.getY()) == true) {
 					// change y position of player 1
@@ -61,7 +60,7 @@ public class Game extends Application {
 					// System.out.println("Up key was pressed");
 				}
 				/**
-				 * key down
+				 * Key down
 				 */
 			} else if (e.getCode() == KeyCode.DOWN) {
 				if (PlayerConstraints.checkUpperBound(player1.getY()) == true) {
@@ -93,8 +92,8 @@ public class Game extends Application {
 				// keeps in bounds
 				// If the ball reaches the left or right border make the step negative
 				if (ball.getLayoutX() <= (WindowConstraints.minimumX + ball.getRadius())
-						|| ball.getLayoutX() >= (WindowConstraints.maximumX - ball.getRadius()) || 
-						PlayerConstraints.checkBallTouchesPlayer(ball, player1, player2))  {
+						|| ball.getLayoutX() >= (WindowConstraints.maximumX - ball.getRadius())
+						|| PlayerConstraints.checkBallTouchesPlayer(ball, player1, player2)) {
 
 					dx = -dx;
 
