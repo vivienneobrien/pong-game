914c73b75e3bb450624782c753db0f278cb6312f
vxo910 <vxo910@student.bhamclear>
2020-04-07 13:52:14 +0100

    bad moving ball

--- src/Game.java
+++ src/Game.java
@@ -1,35 +1,43 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Bounds;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Game extends Application {
 
-	public Ellipse ball;
+	public Circle ball;
 	public Rectangle player1;
 	public Rectangle player2;
 	public Scene scene;
+	public Group root;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	public void setUp(Stage primaryStage) {
-		Group root = new Group();
+		root = new Group();
 
 		// CENTRE X, CENTRE Y, RADIUS X, RADIUS Y
-		ball = new Ellipse(200, 100, 10, 10);
-		ball.setFill(Color.WHITE);
+		ball = new Circle(10, Color.WHITE);
+		ball.relocate(200, 200);
 		player1 = new Rectangle(20, 200, PlayerParameters.radiusX, PlayerParameters.radiusY);
 		player1.setFill(Color.BLUE);
 		player2 = new Rectangle(370, 200, PlayerParameters.radiusX, PlayerParameters.radiusY);
 		player2.setFill(Color.BLUE);
-		
+
 		root.getChildren().add(ball);
 		root.getChildren().add(player1);
 		root.getChildren().add(player2);
@@ -41,7 +49,7 @@ public class Game extends Application {
 	}
 
 	public void startGame() {
-
+		
 		scene.setOnKeyPressed(e -> {
 			/**
 			 * key up
@@ -64,6 +72,45 @@ public class Game extends Application {
 				// System.out.println("Not the right key");
 			}
 		});
+		
+		/**
+		 * Circle movement
+		 */
+		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(20), 
+                new EventHandler<ActionEvent>() {
+
+        	double dx = 7; //Step on x or velocity
+        	double dy = 3; //Step on y
+        	
+            @Override
+            public void handle(ActionEvent t) {
+            	//move the ball
+            	ball.setLayoutX(ball.getLayoutX() + dx);
+            	ball.setLayoutY(ball.getLayoutY() + dy);
+
+                Bounds bounds = root.getBoundsInLocal();
+                
+                //If the ball reaches the left or right border make the step negative
+                if(ball.getLayoutX() <= (bounds.getMinX() + ball.getRadius()) || 
+                        ball.getLayoutX() >= (bounds.getMaxX() - ball.getRadius()) ){
+
+                	dx = -dx;
+
+                }
+
+                //If the ball reaches the bottom or top border make the step negative
+                if((ball.getLayoutY() >= (bounds.getMaxY() - ball.getRadius())) || 
+                        (ball.getLayoutY() <= (bounds.getMinY() + ball.getRadius()))){
+
+                	dy = -dy;
+
+                }
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
+	
 
 	}
 
